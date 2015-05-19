d37a15bdc60278a830b35a2ba699fb972d1a0688
Stuart Attenborrow <stuarta0@gmail.com>
2015-05-19 14:24:51 +1000

    Update FieldPathCombo.cs
    Fixed the issue where the combo wouldn't show the correct field as selected item when drilling through.  This was caused by DataSource and SelectedItem being set before the control was added to the parent container thus somewhere in the depths of the magical WinForms it didn't stick.

--- lib/dbqf.WinForms/FieldPathCombo.cs
+++ lib/dbqf.WinForms/FieldPathCombo.cs
@@ -23,9 +23,11 @@ namespace dbqf.WinForms
 
         void ComboSource_ListChanged(object sender, ListChangedEventArgs e)
         {
-            // assume the entire list is changed each time
-            //layoutFieldPaths.SuspendLayout();
-            for (int i = layoutFieldPaths.Controls.Count - 1; i >= 0; i--)
+            // find at what point the BindingSources changed
+            int from = 0;
+            for (; from < layoutFieldPaths.Controls.Count && from < Adapter.ComboSource.Count && ((ComboBox)layoutFieldPaths.Controls[from]).DataSource == Adapter.ComboSource[from]; from++) ;
+
+            for (int i = layoutFieldPaths.Controls.Count - 1; i >= from; i--)
             {
                 var cbo = (ComboBox)layoutFieldPaths.Controls[i];
                 cbo.SelectedIndexChanged -= cboFields_SelectedIndexChanged;
@@ -33,21 +35,20 @@ namespace dbqf.WinForms
                 layoutFieldPaths.RowStyles.RemoveAt(i);
                 cbo.Dispose();
             }
-            for (int i = 0; i < Adapter.ComboSource.Count; i++)
+            for (int i = from; i < Adapter.ComboSource.Count; i++)
             {
                 var cbo = new ComboBox();
-                cbo.DataSource = Adapter.ComboSource[i];
-                cbo.SelectedItem = Adapter.SelectedPath[i];
-                cbo.SelectedIndexChanged += cboFields_SelectedIndexChanged;
-
                 cbo.DropDownStyle = ComboBoxStyle.DropDownList;
                 cbo.DisplayMember = "DisplayName";
                 cbo.Anchor = AnchorStyles.Left | AnchorStyles.Right;
 
                 layoutFieldPaths.RowStyles.Add(new RowStyle(SizeType.AutoSize));
                 layoutFieldPaths.Controls.Add(cbo);
+
+                cbo.DataSource = Adapter.ComboSource[i];
+                cbo.SelectedItem = Adapter.SelectedPath[i];
+                cbo.SelectedIndexChanged += cboFields_SelectedIndexChanged;
             }
-            //layoutFieldPaths.ResumeLayout();
         }
 
         private void cboFields_SelectedIndexChanged(object sender, EventArgs e)
