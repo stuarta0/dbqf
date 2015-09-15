10a83ca58857ec1c202bec8dc8633f73904a81fc
Stuart Attenborrow <stuarta0@gmail.com>
2015-09-15 22:36:43 +1000

    Added WPF column formatting
    Not a completely clean separation of UI to logic but it's a start -
    result column formats are now provided by the FieldPath from the
    configuration.

--- lib/Application/Standalone.WPF/MainWindow.xaml.cs
+++ lib/Application/Standalone.WPF/MainWindow.xaml.cs
@@ -26,20 +26,24 @@ namespace Standalone.WPF
             InitializeComponent();
             Adapter = adapter;
             this.DataContext = Adapter;
+        }
 
-            //var adorner = new LoadingAdorner(ResultGrid);
-            //var layer = AdornerLayer.GetAdornerLayer(ResultGrid);
-
-            //Adapter.PropertyChanged += (s, e) =>
-            //    {
-            //        if ("IsSearching".Equals(e.PropertyName))
-            //        {
-            //            if (Adapter.IsSearching)
-            //                layer.Add(adorner);
-            //            else
-            //                layer.Remove(adorner);
-            //        }
-            //    };
+        private void ResultGrid_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            if (e.Column is DataGridBoundColumn)
+            {
+                var column = (DataGridBoundColumn)e.Column;
+                
+                var data = ((System.Windows.Controls.DataGrid)sender).ItemsSource as System.Data.DataView;
+                if (data != null)
+                {
+                    if (data.Table.Columns.Contains(e.PropertyName))
+                    {
+                        var path = (dbqf.Criterion.IFieldPath)data.Table.Columns[e.PropertyName].ExtendedProperties["FieldPath"];
+                        column.Binding.StringFormat = path.Last.DisplayFormat;
+                    }
+                }
+            }
         }
     }
 }
