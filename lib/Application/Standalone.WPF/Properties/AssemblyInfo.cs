560e7bd58efc930eb89e8b18549ddf24a3b04543
Stuart Attenborrow <stuarta0@gmail.com>
2016-02-26 22:14:06 +1100

    Updated versions for release

--- lib/Application/Standalone.WPF/Properties/AssemblyInfo.cs
+++ lib/Application/Standalone.WPF/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Stuart Attenborrow")]
 [assembly: AssemblyProduct("Standalone.WPF")]
-[assembly: AssemblyCopyright("Copyright © Stuart Attenborrow 2015")]
+[assembly: AssemblyCopyright("Copyright © Stuart Attenborrow 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
