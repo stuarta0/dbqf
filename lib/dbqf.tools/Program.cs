cadf0f32f0fcadac9fbcedae2df00b2ddcb066e1
Stuart Attenborrow <sattenborrow@pittsh.com.au>
2017-04-12 16:53:34 +1000

    Added project output to dbqf.tools

--- lib/dbqf.tools/Program.cs
+++ lib/dbqf.tools/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using NDesk.Options;
 using System.Xml.Serialization;
+using System.Data.SqlClient;
 
 namespace dbqf.tools
 {
@@ -13,21 +14,66 @@ namespace dbqf.tools
         static void Main(string[] args)
         {
             string connection = "";
+            bool exportProject = false;
+            int listIf = 0;
             var options = new OptionSet() {
-                { "connection=", v => connection = v }
+                { "connection=", "Connection to an SQL Server database that will be used to generate a configuration",
+                    v => connection = v },
+                { "as-project", "Output a project object for use with the standalone application rather than just a configuration (default)",
+                    v => exportProject = (v != null) },
+                { "list=", "Create lists to back fields if the number of unique values in the field exceed LIST",
+                    v => listIf = Int32.Parse(v) }
             };
 
             options.Parse(args);
-            var config = new SqlServerParser(connection).CreateConfiguration();
+            var builder = new SqlConnectionStringBuilder(connection);
+            if (builder.InitialCatalog == null || builder.DataSource == null)
+            {
+                Console.Error.WriteLine("No valid connection string given.");
+                return;
+            }
 
+            var config = new SqlServerParser(connection).CreateConfiguration();
+            if (listIf > 0)
+            {
+                // calculate whether a field should have a list backing it
+            }
+            
             var assembler = new dbqf.Serialization.Assemblers.MatrixConfigurationAssembler(
                 new dbqf.Serialization.Assemblers.SubjectAssembler(
                     new dbqf.Serialization.Assemblers.FieldAssembler(
                         new dbqf.Serialization.Assemblers.Parsers.ParserAssembler())));
 
-            var dto = assembler.Create(config);
-            var xml = new XmlSerializer(dto.GetType());
-            xml.Serialize(System.Console.Out, dto);
+            if (exportProject)
+            {
+                var projAssembler = new Standalone.Core.Serialization.Assemblers.ProjectAssembler(assembler);
+
+                var project = new Standalone.Core.Project()
+                {
+                    Id = Guid.NewGuid(),
+                    Title = builder.InitialCatalog,
+                    Configuration = config,
+                    Connections = new List<Standalone.Core.ProjectConnection>()
+                    {
+                        new Standalone.Core.SqlProjectConnection()
+                        {
+                            Identifier = "initial",
+                            ConnectionString = connection,
+                            DisplayName = connection
+                        }
+                    }
+                };
+
+                var dto = projAssembler.Create(project);
+                var xml = new XmlSerializer(dto.GetType());
+                xml.Serialize(System.Console.Out, dto);
+            }
+            else
+            {
+                var dto = assembler.Create(config);
+                var xml = new XmlSerializer(dto.GetType());
+                xml.Serialize(System.Console.Out, dto);
+            }
         }
     }
 }
